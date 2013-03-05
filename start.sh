f8235565c5a4143e98832d571e6fdc12aeb6e206
Flavio Figueiredo <flaviovdf@gmail.com>
2013-03-05 17:23:32 -0300

    Start script

---
+++ start.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Script to start the web server. Just calls the python code which
+# will take care of the rest. Logs are redirected to the log 
+# folder (folder where app is / log)
+
+function findself() {
+    echo `dirname $0`
+}
+
+OK=0
+ERROR=1
+SELFDIR=`findself`
+LOGSDIR=$SELFDIR/logs
+
+echo "Attempting to start server with params: $*"
+
+if [ -e $LOGSDIR ]; then
+    if [ -d $LOGSDIR ]; then
+        echo "Log folder exists, appending"
+    else
+        echo "Log folder is apparently a file. Exiting"
+        exit $ERROR
+    fi
+else
+    echo "Creating log folder"
+    mkdir $LOGSDIR
+fi
+
+PYTHONPATH=$PYTHONPATH/:$SELFDIR/src/ python -m yourank.deployer $* \
+    2>> $LOGSDIR/yourank.log
+
+if [ $? -eq $OK ]; then
+    echo "Exiting!"
+else
+    echo "Some error occurred, see the error log"
+fi
