46d2780da3157fea4d33cd4ec4e006414e681d13
Flavio Figueiredo <flaviovdf@gmail.com>
2013-03-06 15:12:36 -0300

    Matching project structure

--- yourank/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf8
-from __future__ import division, print_function
-'''
-A simple web application built so that users can
-rank Youtube videos based on their perception of 
-future popularity. Used for academic research on
-a mechanical turk setting.
-'''
