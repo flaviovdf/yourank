836ac017052362b660c1b6842fcae836cb5462d2
Flavio Figueiredo <flaviovdf@gmail.com>
2013-06-28 09:39:25 -0300

    Added popularity restriction

--- pairs/find_good_videos_q1.py
+++ pairs/find_good_videos_q1.py
@@ -8,8 +8,9 @@ import numpy as np
 import urllib2
 
 MIN_VIEWS = 5000 #videos must have at least this many views
-FOLDS = 10
-PER_FOLD = 6
+FOLDS = 6
+PER_FOLD = 10
+GT = .5
 
 #Loading data
 X = np.genfromtxt('tseries.dat')[:, 1:] + 1e-6
@@ -18,6 +19,7 @@ sum_views = X.sum(axis=1)
 
 to_compare_idx = (sum_views >= MIN_VIEWS)
 to_compare = set(ids[to_compare_idx])
+id_to_pop = dict((id_, pop) for id_, pop in zip(ids, sum_views))
 
 #Counting number of time each video appears
 possible = []
@@ -28,7 +30,10 @@ with open('possible.pairs') as pairs_file:
         vid2 = spl[1]
         
         if vid1 in to_compare and vid2 in to_compare:
-            possible.append((vid1, vid2))
+            pop1 = id_to_pop[vid1]
+            pop2 = id_to_pop[vid2]
+            if abs(pop1 - pop2) / max(pop1, pop2) >= GT:
+                possible.append((vid1, vid2))
 
 shuffle(possible)
 total_vids = PER_FOLD * FOLDS
@@ -53,6 +58,7 @@ for pair in possible:
         num_pairs += 1
         if num_pairs == total_vids:
             break
+
         used.add(vid1)
         used.add(vid2)
     except Exception as e:
